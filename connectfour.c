d8c84daefd1e1dfde393a473c2b2aa0ba87f511e
Lucaseven <141701127+Lucaseven@users.noreply.github.com>
2025-10-07 10:34:04 +0900

    Update connectfour.c

--- connectfour.c
+++ connectfour.c
@@ -3,13 +3,14 @@
 #include <locale.h>
 char board[6][7];
 int roundNumber = 0;
+int lastPlayer;
 //prints state of the board
 void printMenu(){
     char cont;
     printf("Welcome to the Connect Four Experience!\n");
     printf("Enter S to Start.\n");
 }
-void printBoard(){
+void printboard(){
     for(int i = 0; i < 6; i++){
         for(int j = 0; j < 7; j++){
             //Check for X
@@ -30,8 +31,53 @@ void printBoard(){
     }
 }
 
-int checkIfWin(int row, int column, int Player){
-    return 0;
+int checkIfWin(int row, int column){
+    char playerPiece = board[row][column];
+    int connected = 1;
+    //check horizontal
+    int c = column;
+    int originalc = c;
+    c++;
+    while (c < 7){
+        if (board[row][c] == playerPiece){
+            connected++;
+        }
+        c++;
+    }
+    c = originalc - 1;
+    while (c > 0){
+        if (board[row][c] == playerPiece){
+            connected++;
+        }
+        c--;
+    }
+    if(connected > 3){
+        return 1;
+    }
+    //check vertical
+    connected = 1;
+    int r = row; 
+    int originalr = row;
+    r++;
+    while(r < 6){
+        if(board[r][column] == playerPiece){
+            connected++;
+        }
+        r++;
+    }
+    r = originalr - 1;
+    while (r > 0){
+        if(board[r][column] == playerPiece){
+            connected ++;
+        }
+        r--;
+    }
+    if(connected > 3){
+        return 1;
+    }
+    //check top left to bottom right diagonal (\)
+    connected = 1;
+    
 }
 
 int main(void){
@@ -41,6 +87,7 @@ int main(void){
             board[i][j] = '-';
         }
     }
-    printBoard();
+    printboard();
     return 0;
 }
+
